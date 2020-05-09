952ca8ecd0a5eb9f160af757bde80364b487e6da
Udit Gaurav <uditgaurav@gmail.com>
2020-05-10 01:26:23 +0530

    --amend
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,40 +2,8 @@
 
 # set -e
 
-# # Extract the base64 encoded config data and write this to the KUBECONFIG
-# mkdir -p /github/workspace/.kube
-# echo $HOME
-# pwd
-# echo "running ls -a"
-# ls -a
-# cat .kube/config
-# echo "running command ls .kube"
-# ls .kube
-# # echo "$KUBE_CONFIG_DATA" | base64 --decode > ${HOME}/.kube/config
-# # export KUBECONFIG=github/home/runner/.kube/config
-
-# # kubectl get nodes
-
-# # cat .kube/config
-
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-
-
-
-echo "RUNNING KUBE CONFIG FROM HOME: RUNNING cat ~/.kube/config "
-cat ~/.kube/config
-echo "RUNNING KUBE CONFIG FROM HOME: RUNNING {HOME}/.kube/config "
-cat ${HOME}/.kube/config
-echo "RUNNING KUBE CONFIG FROM HOME: RUNNING {val}/.kube/config "
-val=$(pwd)
-cat ${val}/.kube/config
-echo "RUNNING KUBE CONFIG FROM HOME: RUNNING cat .kube/config "
-cat .kube/config
-echo "RUNNING KUBE CONFIG FROM HOME: RUNNING echo KUBE_CONFIG_DATA "
-echo "$KUBE_CONFIG_DATA"
-
-
 mkdir -p ${HOME}/.kube
+KUBE_CONFIG_DATA=$(<.kube/config)
 echo "$KUBE_CONFIG_DATA" > ${HOME}/.kube/config
 export KUBECONFIG=${HOME}/.kube/config
 
@@ -53,17 +21,17 @@ cd central-ci
 echo "running dependencies"
 kubectl get nodes
 
-# ##Install litmus if it is not already installed
-# if [ $INSTALL_LITMUS = true ]
-# then
-#   go test tests/install-litmus_test.go -v -count=1
-# fi
+##Install litmus if it is not already installed
+if [ $INSTALL_LITMUS = true ]
+then
+  go test tests/install-litmus_test.go -v -count=1
+fi
 
-# ##Running the chaos experiment template
-# go test tests/${EXPERIMENT_NAME}_test.go -v -count=1
+##Running the chaos experiment template
+go test tests/${EXPERIMENT_NAME}_test.go -v -count=1
 
-# ##litmus cleanup
-# if [ $LITMUS_CLEANUP = true ]
-# then
-#   go test tests/uninstall-litmus_test.go -v -count=1
-# fi
+##litmus cleanup
+if [ $LITMUS_CLEANUP = true ]
+then
+  go test tests/uninstall-litmus_test.go -v -count=1
+fi
