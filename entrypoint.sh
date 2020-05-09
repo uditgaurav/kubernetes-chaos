ffcdab81e816b11b31cf22c642b4c4beb185baae
Udit Gaurav <uditgaurav@gmail.com>
2020-05-09 23:47:58 +0530

    --amend
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,40 @@
 
 set -e
 
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-mkdir -p /github/workspace/.kube
-echo $HOME
-pwd
-echo "running ls -a"
-ls -a
-echo "running command ls .kube"
-ls .kube
-# echo "$KUBE_CONFIG_DATA" | base64 --decode > ${HOME}/.kube/config
-# export KUBECONFIG=github/home/runner/.kube/config
-
-# kubectl get nodes
-
+# # Extract the base64 encoded config data and write this to the KUBECONFIG
+# mkdir -p /github/workspace/.kube
+# echo $HOME
+# pwd
+# echo "running ls -a"
+# ls -a
 # cat .kube/config
+# echo "running command ls .kube"
+# ls .kube
+# # echo "$KUBE_CONFIG_DATA" | base64 --decode > ${HOME}/.kube/config
+# # export KUBECONFIG=github/home/runner/.kube/config
+
+# # kubectl get nodes
+
+# # cat .kube/config
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+mkdir -p ${HOME}/.kube
+echo "$KUBE_CONFIG_DATA" > ${HOME}/.kube/config
+export KUBECONFIG=${HOME}/.kube/config
+
+#Setup 
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+mkdir -p $HOME/go/src/github.com/uditgaurav
+rsync -az --delete ${GOPATH}/src/github.com/uditgaurav/
+cd ${GOPATH}/src/github.com/uditgaurav/
+git clone https://github.com/uditgaurav/central-ci.git
+cd central-ci
 
 #Setup
-export GOPATH=$HOME/runner/go
+export GOPATH=$pwd/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-mkdir -p $HOME/runner/go/src/github.com/uditgaurav
+mkdir -p $pwd/go/src/github.com/uditgaurav
 cd ${GOPATH}/src/github.com/uditgaurav/
 git clone https://github.com/uditgaurav/central-ci.git
 cd central-ci
