a7f488666ea919f6b94eb00fc9c6cfcff5ff2b9e
Udit Gaurav <uditgaurav@gmail.com>
2020-05-10 03:16:25 +0530

    --amend
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,6 @@ value=`cat .kube/config`
 echo "$value" > ${HOME}/.kube/config
 export KUBECONFIG=${HOME}/.kube/config
 
-echo "****************Cating .kube/config **********************"
-cat .kube/config
-echo "****************Cating HOME .kube/config **********************"
-cat ${HOME}/.kube/config
-echo "****************Echoing kube config**********************"
-echo "$KUBECONFIG"
-echo "****************Echoing kube config**********************"
-echo "$KUBECONFIG"
-echo "****************Echoing kube data**********************"
-echo "$KUBE_CONFIG_DATA"
-echo "****************Echoing kubectl get no**********************"
-kubectl get nodes
 #Setup 
 export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
@@ -27,22 +15,21 @@ rsync -az --delete ${GOPATH}/src/github.com/uditgaurav/
 cd ${GOPATH}/src/github.com/uditgaurav/
 git clone https://github.com/uditgaurav/central-ci.git
 cd central-ci
-echo "****************Echoing kubectl get no**********************"
-pwd
-echo "****************Echoing kubectl get node again**********************"
+
+#Getting the nodes of the cluster
 kubectl get nodes
 
-# ##Install litmus if it is not already installed
-# if [ $INSTALL_LITMUS = true ]
-# then
-#   go test tests/install-litmus_test.go -v -count=1
-# fi
+##Install litmus if it is not already installed
+if [ $INSTALL_LITMUS = true ]
+then
+  go test tests/install-litmus_test.go -v -count=1
+fi
 
-# ##Running the chaos experiment template
-# go test tests/${EXPERIMENT_NAME}_test.go -v -count=1
+##Running the chaos experiment template
+go test tests/${EXPERIMENT_NAME}_test.go -v -count=1
 
-# ##litmus cleanup
-# if [ $LITMUS_CLEANUP = true ]
-# then
-#   go test tests/uninstall-litmus_test.go -v -count=1
-# fi
+##litmus cleanup
+if [ $LITMUS_CLEANUP = true ]
+then
+  go test tests/uninstall-litmus_test.go -v -count=1
+fi
