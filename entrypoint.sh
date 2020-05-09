01111fd1fe2ada6509e7574090cbfce556bb1eff
Udit Gaurav <uditgaurav@gmail.com>
2020-05-10 04:39:34 +0530

    --amend
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,8 @@ fi
 ##Running the chaos experiment template
 go test tests/${EXPERIMENT_NAME}_test.go -v -count=1
 
-##litmus cleanup
-if [ $LITMUS_CLEANUP = true ]
-then
-  go test tests/uninstall-litmus_test.go -v -count=1
-fi
+# ##litmus cleanup
+# if [ $LITMUS_CLEANUP = true ]
+# then
+#   go test tests/uninstall-litmus_test.go -v -count=1
+# fi
