1a4b6fde5fb5a288ce12b8a63163b2d9e46fe3c8
Udit Gaurav <uditgaurav@gmail.com>
2020-06-20 04:41:29 +0530

    Add parallel experiments to run in GKE cluster
    Signed-off-by: Udit Gaurav <uditgaurav@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,8 @@ mkdir -p ${HOME}/.kube
 echo "$KUBE_CONFIG_DATA" | base64 --decode > ${HOME}/.kube/config
 export KUBECONFIG=${HOME}/.kube/config
 
-## Setup 
+## Setup
+go get github.com/onsi/ginkgo/ginkgo
 mkdir -p $HOME/go/src/github.com/mayadata-io
 cd ${GOPATH}/src/github.com/mayadata-io/
 dir=${GOPATH}/src/github.com/mayadata-io/chaos-ci-lib
@@ -23,23 +24,22 @@ fi
 cd chaos-ci-lib
 
 ## Install litmus if it is not already installed
-if [ "$INSTALL_LITMUS" = "true" ]; then
-  cd litmus
-  go test install-litmus_test.go -v -count=1 
+if [[ "$INSTALL_LITMUS" == "true" ]]; then
+  go test litmus/install-litmus_test.go -v -count=1 
 fi
 
-if [ "$EXPERIMENT_NAME" == "all" ]; then
+if [[ "$EXPERIMENT_NAME" == "all" ]]; then
 ## Run all BDDs 
 cd tests
 ginkgo -nodes=${PARALLEL_EXECUTION}
+cd ..
 
 else
 ## Run the selected chaos experiment template
-cd ..
 go test tests/${EXPERIMENT_NAME}_test.go -v -count=1 -timeout=${TEST_TIMEOUT}s
 fi
 
 ## Litmus cleanup
-if [ "$LITMUS_CLEANUP" = "true" ]; then
+if [[ "$LITMUS_CLEANUP" == "true" ]]; then
   go test litmus/uninstall-litmus_test.go -v -count=1
 fi
